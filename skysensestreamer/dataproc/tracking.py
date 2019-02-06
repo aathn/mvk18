aebf53f888e8f279c2c2670f0f3c01c91499bb98
Anders Thuné <athune@kth.se>
2019-02-06 22:33:46 +0100

    Update language and formatting

--- skysensestreamer/dataproc/tracking.py
+++ skysensestreamer/dataproc/tracking.py
@@ -1,28 +1,34 @@
 """ tracking.py
 
-Denna fil är del av projektet fr24/mvk18.
-Skapad 2019-02-06 av Anders Ågren Thuné.
-Uppdaterad 2019-02-06 av Anders Ågren Thuné.
+This file is part of the project fr24/mvk18.
+Created 2019-02-06 by Anders Ågren Thuné.
+Last updated 2019-02-06 by Anders Ågren Thuné.
 
-Den här filen innehåller funktioner för styrning av hur kameran ska prioritera
-flygplan, hantering av sikt och extrapolering av flygplanspositioner.
+This file contains functions for controlling how the camera should prioritize
+tracking different planes, handling of sight and extrapolation of airplane
+positions.
 
-Eventuellt bör vi göra en klass som har en lista med flygplan inom räckvidd
-och som håller koll på extrapolerade positioner och synlighetsstatus för varje.
+We might want to make a class keeping a list of airplanes within reach along
+with their extrapolated position and visibility status.
 """
 
 import numpy as np
 import numpy.linalg as la
 
-def extrapolate (positions, times):
-    """
-    En funktion som tar en uppsättning av tidigare positioner och tider och
-    producerar en funktion av tiden som returnerar en extrapolerad position.
 
-    positions: n x 3 array med positionsdata
-    times: n x 1 array med tider
+def extrapolate(positions, times):
     """
-    if not (isinstance(np.ndarray, positions) and isinstance(np.ndarray, times)):
+    extrapolate (positions, times)
+
+    positions: n x 3 array of positional data
+    times: n x 1 array of times belonging to the positions
+
+    Produce a function giving the extrapolated position for a given time,
+    based on the observations in the input. The extrapolation is
+    linear and uses numpy's least squares function.
+    """
+    if not (isinstance(np.ndarray, positions)
+            and isinstance(np.ndarray, times)):
         try:
             positions = np.array(positions)
             times = np.array(times)
@@ -34,5 +40,5 @@ def extrapolate (positions, times):
         raise ValueError("Position array must be n x 3!")
 
     direction, intercept = la.lstsq(
-        np.hstack([times, np.ones(len(times))]), positions, rcond = None)[0]
-    return lambda t: intercept + t*direction
+        np.hstack([times, np.ones(len(times))]), positions, rcond=None)[0]
+    return lambda t: intercept + t * direction
