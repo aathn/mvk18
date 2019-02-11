5ff2491176184aacc4dc7d136084f4a1564ec63a
Anders Thuné <athune@kth.se>
2019-02-11 16:49:39 +0100

    Remove input checks and fix bug

--- skysensestreamer/dataproc/tracking.py
+++ skysensestreamer/dataproc/tracking.py
@@ -21,24 +21,16 @@ def extrapolate(
     based on the observations in the input. The extrapolation is
     linear and uses numpy's least squares function.
 
-    :param positions: n x 3 array of positional data
+    :param positions: n x k array of positional data (k = 2 for camera angles)
     :param times: n x 1 array of times belonging to the positions
     :returns: function producing the position at time t
 
     """
-    if not (isinstance(np.ndarray, positions) and isinstance(np.ndarray, times)):
-        try:
-            positions = np.array(positions)
-            times = np.array(times)
-        except:
-            raise TypeError("Input must be in np array compatible format!")
-    if not positions.shape[0] == times.shape[0]:
-        raise ValueError("Input arrays must have the same length!")
-    if len(positions.shape) != 2 or positions[1] != 3:
-        raise ValueError("Position array must be n x 3!")
+    positions = np.array(positions)
+    times = np.array(times)
 
     direction, intercept = la.lstsq(
-        np.hstack([times, np.ones(len(times))]), positions, rcond=None
+        np.vstack([times, np.ones(len(times))]).transpose(), positions, rcond=None
     )[0]
 
     return lambda t: intercept + t * direction
