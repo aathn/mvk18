dd8f3a46539b354d20fcd5b77aa5a864b3b1d67a
Anders Thuné <athune@kth.se>
2019-02-21 18:20:54 +0100

    Add horizontal bearing method

--- skysensestreamer/dataproc/coords.py
+++ skysensestreamer/dataproc/coords.py
@@ -6,39 +6,25 @@ import numpy as np
 import numpy.linalg as la
 
 
-def to_polar(camera, position: np.ndarray) -> (float, float):
-    """ Compute the angle that the camera should face to capture a plane
 
-    Based on a `draft <https://docs.google.com/document/d/1MtJNY73QoCYKpOv9H7nnySst_zIJB9eUzAs19oHi3uQ)>`_ by Theo Puranen Åhfeldt.
 
-    :param camera: object recording the location of the camera (tentative)
-    :param position: 1 x 3 numpy array or list [latitude, longitude, altitude]
-    :returns: horizontal and vertical angles for the camera to face
+def _horizontal_bearing(self, target: GPSCoords) -> float:
+    """ Calculate the horizontal bearing between self and the target point.
 
+    Get the horizontal bearing, using the following formula:
+        θ = atan2(sin(Δlong).cos(target.lat),
+                  cos(self.lat).sin(target.lat) − sin(self.lat).cos(target.lat).cos(Δlong))
+
+    :param target: GPS coordinates for the target point.
+    :returns: The horizontal bearing in radians
     """
-    if not isinstance(np.ndarray, position):
-        try:
-            position = np.array(position)
-        except:
-            raise TypeError("Input must be in np array compatible format!")
-    if not position.shape[0] == 3:
-        raise ValueError("Expected position as [lat, long, alt]!")
 
-    delta = position - camera.position  # delta = [dx, dy, dz]
-    flat_distance = la.norm(delta[:-1])
+    d_long = self.long - target.long
 
-    hrs_angle = np.pi / 2
-    if delta[0] != 0:
-        hrs_angle = np.arctan(delta[1] / delta[0])
-        if delta[0] < 0:
-            hrs_angle += np.pi
-    elif delta[1] < 0:
-        hrs_angle = -np.pi / 2
+    x = np.sin(d_long) * np.cos(target.lat)
+    y = np.cos(self.lat) * np.sin(target.lat) - (
+        np.sin(self.lat) * np.cos(target.lat) * np.cos(d_long)
+    )
 
-    vrt_angle = np.pi / 2
-    if delta[1] != 0:
-        vrt_angle = np.arctan(delta[2] / flat_distance)
-    elif delta[2] < 0:
-        vrt_angle = -np.pi / 2
-
-    return (hrs_angle, vrt_angle)
+    return (np.atan2(x, y) + 2 * np.pi) % 2 * np.pi
+    """
