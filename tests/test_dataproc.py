52cd4b3727ffb48b4a124ca6d781f8978b0d297c
Anders Thuné <athune@kth.se>
2019-02-28 17:12:39 +0100

    Add tests and bugfixes for coords.py

--- tests/test_dataproc.py
+++ tests/test_dataproc.py
@@ -4,6 +4,9 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import skysensestreamer.dataproc.util as util
+import skysensestreamer.dataproc.coords as coords
+from skysensestreamer.dataproc.coords import GPSCoord
+
 import unittest
 import numpy as np
 
@@ -17,6 +20,48 @@ class ExtrapolateTests(unittest.TestCase):
         extrapolated = util.extrapolate(ts, np.vstack([lin_f(t) for t in ts]))
         for t in range(2, 10):
             self.assertTrue(np.allclose(extrapolated(t), lin_f(t)))
+
+
+class CoordTests(unittest.TestCase):
+    def setUp(self):
+        self.zero_coord = GPSCoord(0.0, 0.0, 0.0)
+
+    def test_get_ecef_x(self):
+        ecef_on_x_axis = GPSCoord(0.0, 0.0, 0.0).get_ecef()
+        self.assertTrue(np.allclose(ecef_on_x_axis[1:], np.zeros(2)))
+        self.assertEqual(ecef_on_x_axis[0], coords.equatorial_radius)
+
+    def test_get_ecef_y(self):
+        ecef_on_y_axis = GPSCoord(0.0, np.pi / 2, 0.0).get_ecef()
+        self.assertTrue(np.allclose(ecef_on_y_axis[[0, 2]], np.zeros(2)))
+        self.assertEqual(ecef_on_y_axis[1], coords.equatorial_radius)
+
+    def test_get_ecef_z(self):
+        ecef_on_z_axis = GPSCoord(np.pi / 2, 0.0, 0.0).get_ecef()
+        self.assertTrue(np.allclose(ecef_on_z_axis[:-1], np.zeros(2)))
+        self.assertEqual(ecef_on_z_axis[2], coords.polar_radius)
+
+    def test_get_ecef_mixed(self):
+        ecef_mixed = GPSCoord(np.pi / 4, np.pi / 4, 0.0).get_ecef()
+        self.assertTrue(np.allclose(ecef_mixed[:-1], np.ones(2) * 10480377))
+        self.assertTrue(np.isclose(ecef_mixed[2], 14722270))
+
+    def test_to_local_for_straight_north(self):
+        north_coord = GPSCoord(np.pi / 4, 0.0, 0.0)
+        local_north = self.zero_coord.to_local(north_coord)
+        self.assertEqual(local_north.azimuth, 0)
+
+    def test_to_local_for_straight_east(self):
+        east_coord = GPSCoord(0.0, np.pi / 2, 0.0)
+        local_east = self.zero_coord.to_local(east_coord)
+        self.assertEqual(local_east.azimuth, np.pi / 2)
+
+    def test_to_local_for_mixed(self):
+        mixed_coord = GPSCoord(np.pi / 4, np.pi / 4, 0.0)
+        local_coord = self.zero_coord.to_local(mixed_coord)
+        self.assertTrue(np.isclose(local_coord.azimuth, 0.61865))
+        self.assertTrue(np.isclose(local_coord.altitude_angle, 2.09488))
+        self.assertTrue(np.isclose(local_coord.distance, 20925520, rtol=0.01))
 
 
 if __name__ == "__main__":
