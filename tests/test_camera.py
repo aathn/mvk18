501cb6a5accd76f7b4b326ed38620bbe6258702a
Markus Wesslén <markus.wesslen@gmail.com>
2019-03-08 00:24:27 +0100

    Rearrange data initialization in setUp

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -6,21 +6,18 @@ from skysensestreamer.dataproc.coords import GPSCoord
 class AirplaneTests(unittest.TestCase):
     def setUp(self):
         self.airplane = Airplane()
-        self.coord1 = GPSCoord(20, 20, 1000)
+
         self.time1 = 1551365010.156895
         self.time1int = int(self.time1)
+        self.coord1 = GPSCoord(20, 20, 700)
 
-        self.coord2 = GPSCoord(21, 21, 900)
         self.time2 = 1551365015.156895
         self.time2int = int(self.time2)
+        self.coord2 = GPSCoord(21, 21, 800)
 
-        self.coord3 = GPSCoord(22, 22, 800)
         self.time3 = 1551365020.156895
         self.time3int = int(self.time3)
-
-        self.coord4 = GPSCoord(23, 23, 850)
-        self.time4 = 1551365027.156895
-        self.time4int = int(self.time4)
+        self.coord3 = GPSCoord(22, 22, 900)
 
     def test_airplane_single_position_extrapolation_is_constant(self):
         self.airplane.append_position(self.time1int, self.coord1)
