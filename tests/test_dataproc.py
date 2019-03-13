89a7947e104d5272c47b3cd7766c62b21825ace7
Anders Thuné <athune@kth.se>
2019-03-13 14:55:01 +0100

    Update coordinate tests to use degrees instead of radians

--- tests/test_dataproc.py
+++ tests/test_dataproc.py
@@ -27,32 +27,32 @@ class CoordTests(unittest.TestCase):
         self.assertEqual(ecef_on_x_axis[0], coords.equatorial_radius)
 
     def test_get_ecef_y(self):
-        ecef_on_y_axis = GPSCoord(0.0, np.pi / 2, 0.0).get_ecef()
+        ecef_on_y_axis = GPSCoord(0.0, 90.0, 0.0).get_ecef()
         self.assertTrue(np.allclose(ecef_on_y_axis[[0, 2]], np.zeros(2)))
         self.assertEqual(ecef_on_y_axis[1], coords.equatorial_radius)
 
     def test_get_ecef_z(self):
-        ecef_on_z_axis = GPSCoord(np.pi / 2, 0.0, 0.0).get_ecef()
+        ecef_on_z_axis = GPSCoord(90.0, 0.0, 0.0).get_ecef()
         self.assertTrue(np.allclose(ecef_on_z_axis[:-1], np.zeros(2)))
         self.assertEqual(ecef_on_z_axis[2], coords.polar_radius)
 
     def test_get_ecef_mixed(self):
-        ecef_mixed = GPSCoord(np.pi / 4, np.pi / 4, 0.0).get_ecef()
+        ecef_mixed = GPSCoord(45.0, 45.0, 0.0).get_ecef()
         self.assertTrue(np.allclose(ecef_mixed[:-1], np.ones(2) * 10480377))
         self.assertTrue(np.isclose(ecef_mixed[2], 14722270))
 
     def test_to_local_for_straight_north(self):
-        north_coord = GPSCoord(np.pi / 4, 0.0, 0.0)
+        north_coord = GPSCoord(45.0, 0.0, 0.0)
         local_north = self.zero_coord.to_local(north_coord)
         self.assertEqual(local_north.azimuth, 0)
 
     def test_to_local_for_straight_east(self):
-        east_coord = GPSCoord(0.0, np.pi / 2, 0.0)
+        east_coord = GPSCoord(0.0, 90.0, 0.0)
         local_east = self.zero_coord.to_local(east_coord)
         self.assertEqual(local_east.azimuth, np.pi / 2)
 
     def test_to_local_for_mixed(self):
-        mixed_coord = GPSCoord(np.pi / 4, np.pi / 4, 0.0)
+        mixed_coord = GPSCoord(45.0, 45.0, 0.0)
         local_coord = self.zero_coord.to_local(mixed_coord)
         self.assertTrue(np.isclose(local_coord.azimuth, 0.61865))
         self.assertTrue(np.isclose(local_coord.altitude_angle, 2.09488))
