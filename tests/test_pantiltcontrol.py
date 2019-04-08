580773c2bf2960d3893f5212f47e311fa763e9ba
Måns Anderssone <mans.andersson97@gmail.com>
2019-04-08 11:05:57 +0200

    Improve import statements

--- tests/test_pantiltcontrol.py
+++ tests/test_pantiltcontrol.py
@@ -1,46 +1,37 @@
 import unittest
-import math
-import skysensestreamer.pantiltcontrol as ptc
+from math import pi
+from skysensestreamer import pantiltcontrol as ptc
 
 
 class PanTiltTests(unittest.TestCase):
     def test_angle_negative(self):
-        self.assertRaises(ValueError, ptc._convert_angle, -10, (0, math.pi), (0, 1000))
+        self.assertRaises(ValueError, ptc._convert_angle, -10, (0, pi), (0, 1000))
 
     def test_pan_angle_greater_than_pi(self):
-        self.assertRaises(
-            ValueError, ptc._convert_angle, math.pi + 0.1, (0, math.pi), (0, 1000)
-        )
+        self.assertRaises(ValueError, ptc._convert_angle, pi + 0.1, (0, pi), (0, 1000))
 
     def test_tilt_angle_greater_than_pi_half(self):
         self.assertRaises(
-            ValueError,
-            ptc._convert_angle,
-            math.pi / 2 + 0.1,
-            (0, math.pi / 2),
-            (0, 1000),
+            ValueError, ptc._convert_angle, pi / 2 + 0.1, (0, pi / 2), (0, 1000)
         )
 
     def test_angle_zero(self):
         lower_bound = 80
         self.assertEqual(
-            ptc._convert_angle(0, (0, math.pi), (lower_bound, lower_bound + 10)),
-            lower_bound,
+            ptc._convert_angle(0, (0, pi), (lower_bound, lower_bound + 10)), lower_bound
         )
 
     def test_pan_angle_pi(self):
         upper_bound = 100
         self.assertEqual(
-            ptc._convert_angle(math.pi, (0, math.pi), (upper_bound - 10, upper_bound)),
+            ptc._convert_angle(pi, (0, pi), (upper_bound - 10, upper_bound)),
             upper_bound,
         )
 
     def test_tilt_angle_pi_half(self):
         upper_bound = 100
         self.assertEqual(
-            ptc._convert_angle(
-                math.pi / 2, (0, math.pi / 2), (upper_bound - 10, upper_bound)
-            ),
+            ptc._convert_angle(pi / 2, (0, pi / 2), (upper_bound - 10, upper_bound)),
             upper_bound,
         )
 
@@ -48,7 +39,7 @@ class PanTiltTests(unittest.TestCase):
         lower_bound = 100
         upper_bound = 200
         self.assertEqual(
-            ptc._convert_angle(math.pi / 2, (0, math.pi), (lower_bound, upper_bound)),
+            ptc._convert_angle(pi / 2, (0, pi), (lower_bound, upper_bound)),
             (upper_bound - lower_bound) / 2 + lower_bound,
         )
 
@@ -56,9 +47,7 @@ class PanTiltTests(unittest.TestCase):
         lower_bound = 100
         upper_bound = 200
         self.assertEqual(
-            ptc._convert_angle(
-                math.pi / 4, (0, math.pi / 2), (lower_bound, upper_bound)
-            ),
+            ptc._convert_angle(pi / 4, (0, pi / 2), (lower_bound, upper_bound)),
             (upper_bound - lower_bound) / 2 + lower_bound,
         )
 
