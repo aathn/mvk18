13ca3b08f8f287d902b498d64035adccc94ba5fc
Anders Thuné <athune@kth.se>
2019-05-23 15:32:58 +0200

    Make executable take position file and flights.js location as args

--- skysensestreamer/__main__.py
+++ skysensestreamer/__main__.py
@@ -2,7 +2,7 @@
 
 This module does the following:
 
-1. Parse the `config file <config.ini>`_.
+1. Parse the `config file <config>`_.
 2. Create a Camera with settings parsed from the config file.
 3. Start a thread with a process that parses and updates flight airplane data.
 
@@ -12,12 +12,17 @@ from skysensestreamer.camera import Camera
 from skysensestreamer.parser import keep_planes_updated, parse_gps_coord
 from threading import Thread, Event
 from configparser import ConfigParser
+import sys
 
 CONFIG_FILE_PATH = "skysensestreamer/config"
 
 config_parser = ConfigParser()
 config_parser.read(CONFIG_FILE_PATH)
-file_locations = config_parser["file_locations"]
+
+if len(sys.argv) >= 3:
+    file_locations = {"flight_data": sys.argv[1], "gps_pos": sys.argv[2]}
+else:
+    file_locations = config_parser["file_locations"]
 camera_settings = config_parser["camera_settings"]
 stream_settings = config_parser["stream_settings"]
 blacklist = config_parser["blacklist"]
